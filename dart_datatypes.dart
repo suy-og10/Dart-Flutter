36266c638c4bc4cc04de72ed4934b42013af873b
Suyog Lokhande <suyoglokhande999@gmail.com>
2025-08-17 17:11:17 +0530

    Second Commit

---
+++ dart_datatypes.dart
@@ -0,0 +1,25 @@
+void main() {
+  // int num1 = 10;
+  // int num2 = 20;
+  // print(num1 + num2);
+  String firstName = "Suyog";
+  String lastName = "Lokhande";
+  print(firstName + " " + lastName);
+
+  // double num1 = 10.5;
+  // double num2 = 20.5;
+  // print(num1 + num2);
+
+  bool isStudent = true;
+  print(isStudent);
+
+  List list1 = ['A', 'B', 'C', 'D'];
+  print(list1);
+
+  Map map1 = {'name': 'Suyog', 'age': 20};
+  print(map1);
+
+  Set set1 = {'A', 'B', 'C', 'D'};
+  print(set1);
+
+}
